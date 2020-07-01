c8f99d7bd2a1916298d9eaf9d3efaf7d66e57817
Steve Kim <dkim.steve@gmail.com>
2020-06-30 21:42:33 -0700

    refactor UserList component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import Loader from "./Loader";
 import UserItem from "./UserItem";
 
-export class UserList extends Component {
-  renderUsers() {
-    return this.props.users.map((user) => (
-      <UserItem key={user.id} user={user} />
-    ));
-  }
+const UserList = ({ users, loading }) => {
+  const renderUsers = () => {
+    return users.map((user) => <UserItem key={user.id} user={user} />);
+  };
 
-  render() {
-    return (
-      <>
-        {this.props.loading ? (
-          <Loader />
-        ) : (
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(3, 1fr)",
-              gridGap: "1rem",
-            }}
-          >
-            {this.renderUsers()}
-          </div>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gridGap: "1rem",
+          }}
+        >
+          {renderUsers()}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default UserList;
