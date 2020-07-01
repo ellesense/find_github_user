93f37f4844d53c79856ab4f3260395868db20344
Steve Kim <dkim.steve@gmail.com>
2020-07-01 15:45:04 -0700

    refactor User component to use react hook

--- src/App.js
+++ src/App.js
@@ -22,21 +22,23 @@ class App extends React.Component {
   onSearch = async (searchKey) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${searchKey}`
+      `https://api.github.com/search/users?q=${searchKey}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
   };
 
   fetchSingleUser = async (username) => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users/${username}`);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ user: res.data, loading: false });
   };
 
   fetchUserRepos = async (username, reposPerPage) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=${reposPerPage}&sort=created:asc`
+      `https://api.github.com/users/${username}/repos?per_page=${reposPerPage}&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ repos: res.data, loading: false });
   };
--- src/components/User.js
+++ src/components/User.js
@@ -1,115 +1,121 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Loader from "./Loader";
 import PropTypes from "prop-types";
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.fetchSingleUser(this.props.match.params.login);
-    this.props.fetchUserRepos(this.props.match.params.login, 4);
-  }
+const User = ({
+  fetchSingleUser,
+  fetchUserRepos,
+  match,
+  loading,
+  user,
+  repos,
+}) => {
+  useEffect(() => {
+    fetchSingleUser(match.params.login);
+    fetchUserRepos(match.params.login, 4);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object,
-    fetchSingleUser: PropTypes.func,
-  };
+  const {
+    name,
+    avatar_url,
+    html_url,
+    location,
+    bio,
+    hireable,
+    login,
+    followers,
+    following,
+    public_repos,
+  } = user;
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      html_url,
-      location,
-      bio,
-      hireable,
-      login,
-      followers,
-      following,
-      public_repos,
-    } = this.props.user;
-
-    return (
-      <>
-        <Link to="/">Back to search</Link>
-        {this.props.loading ? (
-          <Loader />
-        ) : (
-          <div
-            className="card"
+  return (
+    <>
+      <Link to="/">Back to search</Link>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div
+          className="card"
+          style={{
+            boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
+            width: "400px",
+            margin: "40px auto",
+          }}
+        >
+          <img
+            src={avatar_url}
+            alt="profile-img"
             style={{
-              boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
-              width: "400px",
-              margin: "40px auto",
+              width: "150px",
+              display: "block",
+              borderRadius: "99px",
+              margin: "auto",
             }}
-          >
-            <img
-              src={avatar_url}
-              alt="profile-img"
-              style={{
-                width: "150px",
-                display: "block",
-                borderRadius: "99px",
-                margin: "auto",
-              }}
-            />
-            <div style={{ padding: "35px" }}>
-              <h3>
-                <b>{name ? name : "No name"}</b>
-              </h3>
-              <p>
-                <i className="fa fa-github" style={{ marginRight: "10px" }}></i>
-                <a href={html_url}>{login}</a>
-              </p>
-              <p>
-                <i
-                  className="fa fa-map-marker"
-                  style={{ marginRight: "10px" }}
-                ></i>
-                {location}
-              </p>
-              <p>
-                <i
-                  className="fa fa-quote-left"
-                  style={{ marginRight: "10px" }}
-                ></i>
-                {bio ? bio : "Nothing to show"}
-                <i
-                  className="fa fa-quote-right"
-                  style={{ marginLeft: "10px" }}
-                ></i>
-              </p>
-              <p>Hireable? {hireable ? "Yes" : "Not at the moment"}</p>
+          />
+          <div style={{ padding: "35px" }}>
+            <h3>
+              <b>{name ? name : "No name"}</b>
+            </h3>
+            <p>
+              <i className="fa fa-github" style={{ marginRight: "10px" }}></i>
+              <a href={html_url}>{login}</a>
+            </p>
+            <p>
+              <i
+                className="fa fa-map-marker"
+                style={{ marginRight: "10px" }}
+              ></i>
+              {location}
+            </p>
+            <p>
+              <i
+                className="fa fa-quote-left"
+                style={{ marginRight: "10px" }}
+              ></i>
+              {bio ? bio : "Nothing to show"}
+              <i
+                className="fa fa-quote-right"
+                style={{ marginLeft: "10px" }}
+              ></i>
+            </p>
+            <p>Hireable? {hireable ? "Yes" : "Not at the moment"}</p>
 
-              {/* Other information */}
-              <div>Followers: {followers}</div>
-              <div>Following: {following}</div>
-              <div>Public Repos: {public_repos}</div>
+            {/* Other information */}
+            <div>Followers: {followers}</div>
+            <div>Following: {following}</div>
+            <div>Public Repos: {public_repos}</div>
 
-              <div>
-                <h3 style={{ marginTop: "12px" }}>Repositories</h3>
-                <ul>
-                  {this.props.repos.map((repo) => {
-                    return (
-                      <li key={repo.id} style={{ listStyle: "none" }}>
-                        <a
-                          href={repo.html_url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {repo.name}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+            <div>
+              <h3 style={{ marginTop: "12px" }}>Repositories</h3>
+              <ul>
+                {repos.map((repo) => {
+                  return (
+                    <li key={repo.id} style={{ listStyle: "none" }}>
+                      <a
+                        href={repo.html_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {repo.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
-        )}
-      </>
-    );
-  }
-}
+        </div>
+      )}
+    </>
+  );
+};
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object,
+  fetchSingleUser: PropTypes.func,
+};
 
 export default User;
