3f5ff142727c6b501f757c616b9c17c30aff97aa
Steve Kim <dkim.steve@gmail.com>
2020-06-30 21:34:59 -0700

    fetching from github api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import axios from "axios";
 import Nav from "./components/Nav";
 import UserList from "./components/UserList";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <>
         <Nav menuOne="Home" menuTwo="About" menuThree="Contact" />
         <div className="container">
           <h1>Find Dat GitHub User</h1>
-          <UserList />
+          <UserList users={this.state.users} loading={this.state.loading} />
         </div>
       </>
     );
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,36 +2,14 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class UserList extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: "user 1",
-        avatar_url: "",
-        html_url: "",
-      },
-      {
-        id: 2,
-        login: "user 2",
-        avatar_url: "",
-        html_url: "",
-      },
-      {
-        id: 3,
-        login: "user 3",
-        avatar_url: "",
-        html_url: "",
-      },
-    ],
-  };
-
   renderUsers() {
-    return this.state.users.map((user) => (
+    return this.props.users.map((user) => (
       <UserItem key={user.id} user={user} />
     ));
   }
 
   render() {
+    console.log(this.props.users);
     return (
       <div
         style={{
